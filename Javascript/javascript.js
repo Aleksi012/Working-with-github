738c3956a818998426fa4054f90d641cbb977f8d
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-29 21:58:33 +0200

    Tehtävä 3 Javascript: keskeneräinen versio

---
+++ Javascript/javascript.js
@@ -0,0 +1,17 @@
+function get_joke_of_the_day() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+	 if (this.readyState == 4 && this.status == 200) {
+	     // Access the result here
+	     alert(this.responseText);
+	 }
+    };
+    xhttp.open("GET", "https://api.jokes.one/jod?category=animal", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.setRequestHeader("X-JokesOne-Api-Secret", "YOUR API HERE");
+    xhttp.send();
+}
+
+get_joke_of_the_day()
+
+console.log(api.openweathermap.org/data/2.5/forecast?id=524901&appid={b58808d62e73184f020459f7a9102654})
