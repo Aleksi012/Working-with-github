a385c1edf093440a2c11629a60a9293af349c67d
Aleksi012 <aleksi.anttila001@gmail.com>
2021-12-29 20:51:05 +0200

    17.11 Oppitunti

---
+++ 17.11/Harjoitus 1/js.js
@@ -0,0 +1,28 @@
+//Harj a
+var aelem =  document.getElementById("content")
+console.log(aelem.nodeName)
+
+//Harj b
+var firstBasic = document.querySelector(".basic")
+console.log(firstBasic.nodeName)
+
+//Harj c
+var allItems = document.getElementsByTagName("li")
+for(i of allItems){
+    console.log(i.textContent)
+}
+
+//Harj d
+let d = document.querySelector("body")
+let allNodes = document.querySelectorAll("*")
+
+for ( x of allNodes ) {
+    console.log(x.nodeName)
+}
+
+//Harj e
+var article = document.querySelector("article")
+
+for(a of article.querySelectorAll("p")){
+    console.log("**" + a.textContent);
+}
